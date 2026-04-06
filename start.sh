69c28bafe9bd675b2f0a53cd58f4a44a814a7036
Abhinav <abhinavpal8960@gmail.com>
2026-04-06 20:14:27 +0530

    changes

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
+
+php artisan config:clear
+php artisan cache:clear
+php artisan migrate --force
+php artisan db:seed --force
+
 exec php artisan serve --host=0.0.0.0 --port=${PORT}
