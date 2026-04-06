5b9a607a06a079124ad44c03265ded168bf89f52
Abhinav <abhinavpal8960@gmail.com>
2026-04-06 19:11:31 +0530

    changes

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'file'),
+    // 'default' => env('CACHE_STORE', 'file'),
+    'default' => 'file',
 
     /*
     |--------------------------------------------------------------------------
