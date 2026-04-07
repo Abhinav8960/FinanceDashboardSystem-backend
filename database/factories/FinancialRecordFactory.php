27382bbb36c71dbee3cd060bb6796bd33f858b83
Abhinav <abhinavpal8960@gmail.com>
2026-04-07 11:48:46 +0530

    add seeder correctly

--- database/factories/FinancialRecordFactory.php
+++ database/factories/FinancialRecordFactory.php
@@ -19,11 +19,11 @@ class FinancialRecordFactory extends Factory
     {
         return [
             'user_id' => \App\Models\User::inRandomOrder()->first()->id,
-            'amount' => fake()->numberBetween(100, 5000),
-            'type' => fake()->randomElement(['income', 'expense']),
+            'amount' => $this->faker->numberBetween(100, 5000),
+            'type' => $this->faker->randomElement(['income', 'expense']),
             'category_id' => \App\Models\Category::inRandomOrder()->first()->id,
-            'description' => fake()->sentence(),
-            'date' => fake()->date(),
+            'description' => $this->faker->sentence(),
+            'date' => $this->faker->date(),
         ];
     }
 }
